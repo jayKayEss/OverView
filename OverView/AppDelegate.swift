aa7faaf5b1f9da9af332222b93bdbe68761a5983
Justin Sheckler <self@jaykayess.com>
2015-11-07 13:28:31 -0500

    Initial Commit

---
+++ OverView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OverView
+//
+//  Created by Justin Sheckler on 11/7/15.
+//  Copyright © 2015 Justin Sheckler. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OverView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OverView
+//
+//  Created by Justin Sheckler on 11/7/15.
+//  Copyright © 2015 Justin Sheckler. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
